54a8296648fc9ad83f89209ec9e1d9c6960ab6f7
koohiikouho <115139996+koohiikouho@users.noreply.github.com>
2025-02-18 09:35:59 +0800

    modified the cards, still needs their IDs hooked to func.js

--- jQuery/func.js
+++ jQuery/func.js
@@ -1,26 +1,31 @@
 
-function cpuShowQueryReturn(cpuName){
-    $('#cpuname').text(cpuName);
-    $('#cpudesc').text("5 times the mambos");
+function removeQueryReturn(itemProdName, itemProdDesc){
+    $(itemProdName).text("");
+    $(itemProdDesc).text("");
+    $(itemProdName).hide();
+    $(itemProdDesc).hide();
+}
 
-    $('#cpuname').show();
-    $('#cpudesc').show();
+function cpuShowQueryReturn(cpuName, cpuDesc){
+    $('#cpuProdName').text(cpuName);
+    $('#cpuDesc').text(cpuDesc);
+
+    $('#cpuProdName').show();
+    $('#cpuDesc').show();
 
 }
 function cpuRemoveQueryReturn(){
-    $('#cpuname').text("");
-    $('#cpuname').hide();
-    $('#cpudesc').hide();
+    $('#cpuProdName').text("");
+    $('#cpuProdName').hide();
+    $('#cpuDesc').hide();
 }
+
+
+
 function cpuQueryReplaceInput() {
-    
-    // insert shit here
-
-    
-
 
     $('#cpuDataList').hide(); //alter this so that it has the info
-    $('#cpuloading').show();
+    $('#cpuLoading').show();
     //you should probably do some php here
 
 
@@ -28,60 +33,139 @@ function cpuQueryReplaceInput() {
     
     //cpu specs should go here into this function after you're done implementing AJAX
 
-    cpuShowQueryReturn("AMD MamboX3D");
+    cpuShowQueryReturn("AMD MamboX3D", "16 times the mambos - Todd Howard");
     
 
     
     
-    $('#cpuloading').hide();
-    $('#addcpubutton').hide();
-    $('#removecpubutton').show();
+    $('#cpuLoading').hide();
+    $('#addCpuButton').hide();
+    $('#remCpuButton').show();
+    //SHOULD IMPLEMENT FUNCTION HERE THAT DISPLAYS IT AT THE SIDEBAR
+
+
+}
+
+function showQueryReturn(itemProdName, itemProdDesc, newProdName, newProdDesc){
+    $(itemProdName).text(newProdName);
+    $(itemProdDesc).text(newProdDesc);
+
+    $(itemProdName).show();
+    $(itemProdDesc).show();
+
+}
+
+// Hides the searching function and shows the FULL Name of the the item
+// as well as the description
+// TODO: IMPLEMENT QUERY HERE
+
+function queryReplaceInput(cardDataList, cardLoading, addButton, remButton,
+    itemProdName, itemProdDesc) {
+
+    $(cardDataList).hide(); 
+    $(cardLoading).show();  //alter this so that it has the info
+
+    
+
+    //you should probably do some php here
+    //make it so that it is variable
+
+    //specs should go here into this function after you're done implementing AJAX
+    showQueryReturn(itemProdName, itemProdDesc, newItemProdName, newItemProdDesc);
+
+    $(cardLoading).hide();
+
+    $(addButton).hide();
+    $(remButton).show();
+    //SHOULD IMPLEMENT FUNCTION HERE THAT DISPLAYS IT AT THE SIDEBAR
+
+
+}
+
+
+
+function queryReplaceText(cardDataList, cardLoading, addButton, remButton, itemProdName, itemProdDesc) {
+    
+    $(cardDataList).show();
+    $(itemProdDesc).hide();
+    $(itemProdName).hide(); // Replace input with text
+
+    removeQueryReturn(itemProdName, itemProdDesc);
+
+    $(remButton).hide();
+    $(addButton).show();
+
 }
 
 function cpuQueryReplaceText() {
 
     $('#cpuDataList').show();
-    $('#description').hide(); // Replace input with text
+    $('#cpuDesc').hide(); // Replace input with text
 
     cpuRemoveQueryReturn();
 
-    $('#removecpubutton').hide();
-    $('#addcpubutton').show();
+    $('#remCpuButton').hide();
+    $('#addCpuButton').show();
 
+}
+
+function unlockMoboMemory(){
+    $('#moboWarning').hide();
+    $('#moboDataList').show();
+    $('#addMoboButton').show();
+}
+
+function lockMoboMemory(){
+    $('#moboWarning').show();
+    $('#moboDataList').hide();
+    $('#addMoboButton').hide();
+    $('#remMoboButton').hide();
 }
 
 function hideAtStart(){
-    $('#removecpubutton').hide();
-    $('#cpuloading').hide();
-    $('#cpuname').hide();
-    $('#cpudesc').hide();
+    //cpu card hide
+    $('#remCpuButton').hide();
+    $('#cpuLoading').hide();
+    $('#cpuProdName').hide();
+    $('#cpuDesc').hide();
+    //mobo card hide
+    $('#remMoboButton').hide();
+    $('#moboLoading').hide();
+    $('#moboDataList').hide();
+    $('#addMoboButton').hide();
+    //memory card hide
+
 }
 
 $(document).ready(function(){
-
-    cpuSearchCount = 0;
+    
     hideAtStart();
 
-    $('#addcpubutton').click(function(){
+    //don't use these or try to replicate it, this shit suckss
+    $('#addCpuButton').click(function(){
         cpuQueryReplaceInput();
-        document.getElementById('mobBrand').disabled = false;
-        document.getElementById('mobChipset').disabled = false;
-        document.getElementById('mobPart').disabled = false;
-        document.getElementById('ramBrand').disabled = false;
-        document.getElementById('ramSize').disabled = false;
-        document.getElementById('ramName').disabled = false;
-        document.getElementById('ramQty').disabled = false;
+        unlockMoboMemory();
     });
 
-    $('#removecpubutton').click(function(){
+    $('#remCpuButton').click(function(){
         cpuQueryReplaceText();
-        document.getElementById('mobBrand').disabled = true;
-        document.getElementById('mobChipset').disabled = true;
-        document.getElementById('mobPart').disabled = true;
-        document.getElementById('ramBrand').disabled = true;
-        document.getElementById('ramSize').disabled = true;
-        document.getElementById('ramName').disabled = true;
-        document.getElementById('ramQty').disabled = true;
+        lockMoboMemory();
+    });    
+
+    //
+
+    $('#addMoboButton').click(function(){
+
+        
+        queryReplaceInput('#moboDataList', '#moboLoading', '#addMoboButton', '#remMoboButton', 
+            '#mobProdName', '#moboDesc');
+
+
+    });    
+
+    $('#remMoboButton').click(function(){
+        queryReplaceText('#moboDataList', '#moboLoading', '#addMoboButton', '#remMoboButton', 
+            '#mobProdName', '#moboDesc');
     });    
 
 });
