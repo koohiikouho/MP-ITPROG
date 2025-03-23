e1e2d36104ede4d4c7295f874a04748643bb8c6c
kingbhatss <nk.reyes1@gmail.com>
2025-03-23 19:10:42 +0800

    CPU ADD DONE

--- admin/js/populateScripts.js
+++ admin/js/populateScripts.js
@@ -14,7 +14,8 @@ function populateCPU(){
 
     xmlhttp.open("GET", "../php/cpuInitBrand.php", true);
     xmlhttp.send();
-    getCPUSockets();
+
+
 };
 
 function getCPUSockets(){
@@ -70,9 +71,54 @@ function popMem(){
 
 
 $(document).ready(function(){
-    
+
     validSessionIDCheck();
     populateCPU();
+    getCPUSockets();
     popMem();
+
+
+    $('#cpuAdd').click(function(){
+        var cpuName = document.getElementById("cpuName").value;
+        var cpuBrand = document.getElementById("cpuBrand");
+        var cpuBrandText = cpuBrand.options[cpuBrand.selectedIndex].value;
+        var cpuCores = document.getElementById("cpuCores").value;
+        var cpuThreads = document.getElementById("cpuThreads").value;
+        var cpuClock = document.getElementById("cpuClock").value;
+        var cpuSocket = document.getElementById("cpuSocket");
+        var cpuSocketText = cpuSocket.options[cpuSocket.selectedIndex].value;
+        var cpuPrice = document.getElementById("cpuPrice").value;
+    
+        // Debugging alerts
+        alert("CPU Name: " + cpuName);
+        alert("CPU Brand: " + cpuBrandText);
+        alert("CPU Cores: " + cpuCores);
+        alert("CPU Threads: " + cpuThreads);
+        alert("CPU Clock: " + cpuClock);
+        alert("CPU Socket: " + cpuSocketText);
+        alert("CPU Price: " + cpuPrice);
+    
+        var xmlhttp = new XMLHttpRequest();
+        xmlhttp.onreadystatechange = function(){
+            if (this.readyState == 4 && this.status == 200) {
+                alert("Response from PHP: " + this.responseText); // Show success or error message
+                document.getElementById("addCPUForm").reset(); // Reset form after submission
+            }
+        };
+    
+        // Construct GET request without encodeURIComponent()
+        xmlhttp.open("GET", "./php/addCPU.php?cpuName=" + cpuName +
+                     "&cpuBrand=" + cpuBrandText +
+                     "&cpuCores=" + cpuCores +
+                     "&cpuThreads=" + cpuThreads +
+                     "&cpuClock=" + cpuClock +
+                     "&cpuSocket=" + cpuSocketText +
+                     "&cpuPrice=" + cpuPrice, true);
+        xmlhttp.send();
+    
+        alert("PHP request sent!");
+    });
+    
+    
 
 });
